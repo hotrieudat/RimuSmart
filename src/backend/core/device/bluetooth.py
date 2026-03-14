b4b6ad10ce4445bb64cdea5e424c0b302d2ba344
hotrieudat <trieudat.ho@gmail.com>
2026-03-14 15:40:55 +0700

    Add bluetooth document

---
+++ src/backend/core/device/bluetooth.py
@@ -0,0 +1,6 @@
+# Scan: bluetoothctl --timeout 10 scan on
+# Pair: bluetoothctl pair 84:26:7A:6C:2E:1F (Ghép đôi).
+# Connect: bluetoothctl connect 84:26:7A:6C:2E:1F (Kết nối).
+# Thiết bị đang kết nối: bluetoothctl devices Connected
+# Disconnect: bluetoothctl disconnect 84:26:7A:6C:2E:1F (Ngắt kết nối).
+# Remove: bluetoothctl remove 84:26:7A:6C:2E:1F (Xóa thiết bị đã ghép đôi).
